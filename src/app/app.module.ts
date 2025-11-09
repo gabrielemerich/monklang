4b98ae215eb568740d3c498a1af87d28fac4986f
Gabriel Emerich <glemerich@gmail.com>
2025-11-09 11:38:50 -0300

    chore: upgrade Angular dependencies to version 18 and refactor HttpClientModule usage in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 
-@NgModule({
-  declarations: [AppComponent, HomeComponent, CategoriesComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, HomeComponent, CategoriesComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule, SharedModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
